cfe51bf0146681ca5bb8fd35ce16eac7196d13e4
suyuanqing <2578448390@qq.com>
2017-05-08 16:23:09 +0800

    Initial commit

---
+++ app/src/main/java/com/usian/android_5_8_task/AS.java
@@ -0,0 +1,8 @@
+package com.usian.android_5_8_task;
+
+/**
+ * Created by ${苏元庆} on 2017/5/8.
+ */
+
+public class AS {
+}
---
+++ app/src/main/java/com/usian/android_5_8_task/MainActivity.java
@@ -0,0 +1,13 @@
+package com.usian.android_5_8_task;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
