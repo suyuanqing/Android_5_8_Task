89fa25e592e404676a2d630600aba6d932450720
suyuanqing <2578448390@qq.com>
2017-05-08 17:05:08 +0800

    更新

--- app/src/main/java/com/usian/android_5_8_task/MainActivity.java
+++ app/src/main/java/com/usian/android_5_8_task/MainActivity.java
@@ -1,13 +1,28 @@
 package com.usian.android_5_8_task;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+                Toast.makeText(MainActivity.this, "你好", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
